b6d5c786481a471f354fa33bd49f6bb5c9ed3e0c
occamskatana <johngallweycarter@gmail.com>
2015-12-20 22:34:44 -0500

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
 	resources :posts
 
-  devise_for :users 
+  devise_for :users, controllers: {
+
+  	registrations: 'registraions'
+  }
+
 
   root  'welcome#index'
   get 'welcome/about'
